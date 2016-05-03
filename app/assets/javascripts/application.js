efab0a4ccb093b183ed9b30258dad844edc97817
Ankita-Viva <ankita.agarwal@vivacityinfotech.com>
2016-05-03 12:03:12 +0530

    js order changed for dropdown issue

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,6 +10,7 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+//= require bootstrap-multiselect
 //= require jquery
 //= require jquery.raty
 //= require jquery.turbolinks
@@ -17,6 +18,5 @@
 //= require ratyrate
 //= require turbolinks
 //= require social-share-button
-//= require bootstrap-multiselect
 //= require ckeditor/init
 //= require_tree .
