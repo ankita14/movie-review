b7cc445694494d70747d44693a01b57d93fa0cd7
Ankita-Viva <ankita.agarwal@vivacityinfotech.com>
2016-05-05 18:30:08 +0530

    mailer for forgot password added

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -69,6 +69,21 @@ Rails.application.configure do
   # the I18n.default_locale when a translation cannot be found).
   config.i18n.fallbacks = true
 
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.default_url_options = { :host => 'https://viva-movie-review.herokuapp.com' }
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default :charset => "utf-8"
+  config.action_mailer.smtp_settings = {
+      :address => "smtp.gmail.com",
+      :port => 587,
+      :domain => 'https://viva-movie-review.herokuapp.com',
+      :user_name => 'vendorfirst@gmail.com',
+      :password => "vivatest123",
+      :authentication => :plain,
+      :enable_starttls_auto => true
+  }
+
   # Send deprecation notices to registered listeners.
   config.active_support.deprecation = :notify
 
