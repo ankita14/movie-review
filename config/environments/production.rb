0e22d4095d51612012b5ce2db3f88eb3190e1b69
Ankita-Viva <ankita.agarwal@vivacityinfotech.com>
2016-05-06 11:01:05 +0530

    mailer settings for heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -70,14 +70,14 @@ Rails.application.configure do
   config.i18n.fallbacks = true
 
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.default_url_options = { :host => 'https://viva-movie-review.herokuapp.com' }
+  config.action_mailer.default_url_options = { :host => 'viva-movie-review.herokuapp.com' }
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.default :charset => "utf-8"
   config.action_mailer.smtp_settings = {
       :address => "smtp.gmail.com",
       :port => 587,
-      :domain => 'https://viva-movie-review.herokuapp.com',
+      :domain => 'gmail.com',
       :user_name => 'vendorfirst@gmail.com',
       :password => "vivatest123",
       :authentication => :plain,
