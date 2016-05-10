5ee3585c0896fe363ad941dfd970656fe79c6059
Ankita-Viva <ankita.agarwal@vivacityinfotech.com>
2016-05-10 19:38:35 +0530

    removed unwanted code

--- lib/rails_admin/front_view.rb
+++ lib/rails_admin/front_view.rb
@@ -15,16 +15,11 @@ module RailsAdmin
 				 register_instance_option :visible? do
 					authorized?
 				end
-				# register_instance_option :visible? do
-				#   authorized? && bindings[:object].class == Movie
-				# end
-				# We want the action on members, not the Users collection
+				
 				register_instance_option :member do
 					true
 				end
-				# register_instance_option :collection do
-				#   true
-				# end
+				
 				register_instance_option :link_icon do
 					'icon-eye-open'
 				end
